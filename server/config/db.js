24658232dd92b495aad177793cd19e51a013b727
Danila Kulinkovich <danek200326@gmail.com>
2025-03-23 01:57:46 +0200

    fix[09]: fix db

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,17 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'mysql.railway.internal',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST || 'yamanote.proxy.rlwy.net', 
+  port: process.env.DB_PORT || 18763, 
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'rLOgGlgRwMepCjPcMZbRTXfJcPYqliRg',
+  password: process.env.DB_PASSWORD || 'nnNuPCVGjUbasqaAsbRlwDDHxxrHOBna',
   database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 module.exports = db;
