78c9351be124c1bd5aadfbd033693d9bcd828178
Danila Kulinkovich <danek200326@gmail.com>
2025-03-23 15:59:47 +0200

    fix[12]: fix db

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,10 @@
-const mysql = require('mysql2');
 require('dotenv').config();
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`;
+const mysql = require('mysql2');
+
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 const db = mysql.createConnection(urlDB);
 
 module.exports = db;
+
+
